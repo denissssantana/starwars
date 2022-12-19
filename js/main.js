e1fbea6aeca9d54b15a1730b06fd5dac3845a1e5
denissssantana <105013785+denissssantana@users.noreply.github.com>
2022-12-19 15:36:10 -0300

    sexto commit construindo paginas

--- js/main.js
+++ js/main.js
@@ -1,43 +1,20 @@
-const url = "https://swapi.dev/api/people" 
+fetch('https://swapi.dev/api/people/1/').then(response =>{
+    return response.json()
+}).then(corpo=>{
+    document.getElementById("nome").innerHTML=corpo.name
+    document.getElementById("altura").innerHTML=corpo.height
+})
 
-const loadingElement = document.querySelector("#loading")
-const peopleContainer = document.querySelector("#people-container")
-
-async function getAllPeople() {
-    const response = await fetch(url); 
-    console.log(response); 
-
-    const data = await response.json(); 
-    console.log(data);
-
-    //loadingElement.classList.add("hide");
-
-    data.map((people) => {
-        const div = document.createElement("div");
-        const species = document.createElement("p");
-        const birth_year = document.createElement("p");
-        const link = document.createElement("a");
-
-
-        species.innerText = people.species;
-        birth_year.innerText = people.birth_year;
-        link.innerText = "Ler";
-        link.setAttribute("href", '/index.html?id=${people.id}');
-
-        div.appendChild(species);
-        div.appendChild(birth_year);
-        div.appendChild(link);
-
-        peopleContainer,appendChild(div);
-
-    });
-}
-
-getAllPeople();
+fetch('https://swapi.dev/api/people/2/').then(response =>{
+    return response.json()
+}).then(corpo=>{
+    document.getElementById("nome2").innerHTML=corpo.name
+    document.getElementById("altura2").innerHTML=corpo.height
+})
 
 
 
 
 
 
-// API - https://swapi.dev/api/people/1/ 
+// API - https://swapi.dev/api/people/ 
